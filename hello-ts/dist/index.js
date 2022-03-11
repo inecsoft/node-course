6a081d17664c2e5512786385cb1472c3592760fd
ivan-arteaga-tfgm <ivan.arteaga@tfgm.com>
2022-03-11 10:03:40 +0000

    first steps with typescripts Fri Mar 11 10:03:40 UTC 2022

---
+++ hello-ts/dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+let x = 7;
+let y = 10;
+const sum = x + y;
+const result = 'The sum of ${x} and ${y} is ${sum}';
+console.log(result);
+// npm init -y
+// npm install typescript --save-dev
+// npm run tsc -- --init
+//# sourceMappingURL=index.js.map
---
+++ hello-ts/index.ts
@@ -0,0 +1,12 @@
+let x: number = 7;
+let y: number = 10;
+const sum : number = x + y;
+const result: string = 'The sum of ${x} and ${y} is ${sum}';
+console.log(result);
+
+// npx --package typescript tsc --outDir dist index.ts
+// npm init -y
+// npm install typescript --save-dev
+// generate tsconfig.json file
+// npm run tsc -- --init
+//# sourceMappingURL=index.js.map
