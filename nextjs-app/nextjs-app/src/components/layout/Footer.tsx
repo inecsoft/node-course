e51702100d53cfc42cbab2cd9613207a59feba76
ivan-arteaga-tfgm <ivan.arteaga@tfgm.com>
2022-05-04 08:59:51 +0000

    update project

---
+++ nextjs-app/nextjs-app/src/components/layout/Footer.tsx
@@ -0,0 +1,14 @@
+// src/components/layout/Footer.tsx
+
+import { SITE_TITLE } from "../../constants/constants"
+
+const Footer = () => {
+  return (
+    <footer className="bg-blue-600 text-white text-center py-4">
+      <p className="mb-1">{SITE_TITLE} &copy;</p>
+      <p>Designed & developed by Inecsoft</p>
+    </footer>
+  )
+}
+
+export default Footer
